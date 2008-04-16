14c3bb939a1bf6d07af95cb3cf21c81d0a2f088c
tanganbo <tanganbo@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2008-04-16 12:49:36 +0000

    Add list log and search tab

--- source/BaseWebManager/Module/FrameWork/SystemApplicationManage/LogEventView/ListPage.aspx.cs
+++ source/BaseWebManager/Module/FrameWork/SystemApplicationManage/LogEventView/ListPage.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Configuration;
 using System.Collections;
@@ -8,22 +9,59 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using Powerasp.Enterprise.Core.BaseManager.BaseDataUIPage.ListFunction;
+using Powerasp.Enterprise.Core.BaseManager.Domain;
+using Powerasp.Enterprise.Core.BaseManager.Service;
+using Powerasp.Enterprise.Core.BaseManager.Web;
+using Powerasp.Enterprise.Core.Web.BasePage;
+using Powerasp.Enterprise.Core.Web.ControlHelper;
 
 namespace BaseWebManager.Module.FrameWork.SystemApplicationManage.LogEventView
 {
-    public partial class ListPage : System.Web.UI.Page
+    public partial class ListPage : SystemLogListUIPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!Page.IsPostBack)
+            {
+                ListBind();
+            }
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
+        private void ListBind()
         {
-            
+            int record = 0;
+            List<SystemLog> list =
+                 systemLogServiceInstance.GetAll(this.Pager.StartRecordIndex - 1, this.Pager.PageSize, out record);
+            DataBindHelper.BindListDataToGridView<SystemLog>(this.systemLogGridView, list);
+            this.Pager.RecordCount = record;
         }
 
+        private void SearchBind()
+        {
+            //int record = 0;
+            //List<SystemLog> list =
+            //     systemLogServiceInstance.GetAll(this.Pager.StartRecordIndex - 1, this.Pager.PageSize, out record);
+            //DataBindHelper.BindListDataToGridView<SystemLog>(this.systemLogGridView, list);
+            //this.Pager.RecordCount = record;
 
+            //this.tabList.ActiveTabIndex = 0;
+        }
 
+        protected void systemLogGridView_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            //this.ProcessGridCmd(sender, e);
+            //ListBind();
+        }
+
+        protected void Pager_PageChanged(object sender, EventArgs e)
+        {
+            ListBind();
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            SearchBind();
+        }
     }
 }
