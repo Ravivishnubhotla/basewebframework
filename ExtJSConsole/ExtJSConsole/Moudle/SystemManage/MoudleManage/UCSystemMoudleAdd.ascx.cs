75653ca79e45d14260806595fb59b39375f3e5cd
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2010-03-28 02:29:15 +0000

    GenerateMoudleEditPage

--- ExtJSConsole/ExtJSConsole/Moudle/SystemManage/MoudleManage/UCSystemMoudleAdd.ascx.cs
+++ ExtJSConsole/ExtJSConsole/Moudle/SystemManage/MoudleManage/UCSystemMoudleAdd.ascx.cs
@@ -4,14 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Coolite.Ext.Web;
+using Legendigital.Framework.Common.BaseFramework.Bussiness.Wrappers;
 
 namespace ExtJSConsole.Moudle.SystemManage.MoudleManage
 {
+    [AjaxMethodProxyID(IDMode = AjaxMethodProxyIDMode.Alias, Alias = "UCSystemMoudleAdd")]
     public partial class UCSystemMoudleAdd : System.Web.UI.UserControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        [AjaxMethod]
+        public void Show()
+        {
+            try
+            {
+                this.winSystemMoudleAdd.Show();
+            }
+            catch (Exception ex)
+            {
+                Coolite.Ext.Web.ScriptManager.AjaxSuccess = false;
+                Coolite.Ext.Web.ScriptManager.AjaxErrorMessage = "错误信息：" + ex.Message;
+            }
+        }
+
+        protected void btnSaveSystemMoudle_Click(object sender, AjaxEventArgs e)
+        {
+            try
+            {
+                SystemMoudleWrapper obj = new SystemMoudleWrapper();
+                obj.MoudleNameCn = this.txtMoudleNameCn.Text.Trim();
+                obj.MoudleNameEn = this.txtMoudleNameEn.Text.Trim();
+                obj.MoudleNameDb = this.txtMoudleNameDb.Text.Trim();
+                obj.MoudleDescription = this.txtMoudleDescription.Text.Trim();
+                //obj.ApplicationID = Convert.ToInt32(this.txtApplicationID.Text.Trim());
+                obj.MoudleIsSystemMoudle = this.chkMoudleIsSystemMoudle.Checked;
+
+
+                SystemMoudleWrapper.Save(obj);
+
+                winSystemMoudleAdd.Hide();
+
+            }
+            catch (Exception ex)
+            {
+                Coolite.Ext.Web.ScriptManager.AjaxSuccess = false;
+                Coolite.Ext.Web.ScriptManager.AjaxErrorMessage = "错误信息：" + ex.Message;
+            }
         }
     }
 }
--- ExtJSConsole/ExtJSConsole/Moudle/SystemManage/MoudleManage/UCSystemMoudleEdit.ascx.cs
+++ ExtJSConsole/ExtJSConsole/Moudle/SystemManage/MoudleManage/UCSystemMoudleEdit.ascx.cs
@@ -2,16 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
+using Coolite.Ext.Web;
+using Legendigital.Framework.Common.BaseFramework.Bussiness.Wrappers;
 
 namespace ExtJSConsole.Moudle.SystemManage.MoudleManage
 {
+    [AjaxMethodProxyID(IDMode = AjaxMethodProxyIDMode.Alias, Alias = "UCSystemMoudleEdit")]
     public partial class UCSystemMoudleEdit : System.Web.UI.UserControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+
+        [AjaxMethod]
+        public void Show(int id)
+        {
+            try
+            {
+                SystemMoudleWrapper obj = SystemMoudleWrapper.FindById(id);
+
+                if (obj != null)
+                {
+                    this.txtMoudleNameCn.Text = obj.MoudleNameCn.ToString();
+                    this.txtMoudleNameEn.Text = obj.MoudleNameEn.ToString();
+                    this.txtMoudleNameDb.Text = obj.MoudleNameDb.ToString();
+                    this.txtMoudleDescription.Text = obj.MoudleDescription.ToString();
+                    this.txtApplicationID.Text = obj.ApplicationID.ToString();
+                    this.chkMoudleIsSystemMoudle.Checked = obj.MoudleIsSystemMoudle;
+
+
+
+                    hidSystemMoudleID.Text = id.ToString();
+
+
+                    winSystemMoudleEdit.Show();
+
+                }
+                else
+                {
+                    ScriptManager.AjaxSuccess = false;
+                    ScriptManager.AjaxErrorMessage = "错误信息：数据不存在";
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Coolite.Ext.Web.ScriptManager.AjaxSuccess = false;
+                Coolite.Ext.Web.ScriptManager.AjaxErrorMessage = "错误信息：" + ex.Message;
+                return;
+            }
+        }
+
+        protected void btnSaveSystemMoudle_Click(object sender, AjaxEventArgs e)
+        {
+            try
+            {
+                SystemMoudleWrapper obj = SystemMoudleWrapper.FindById(int.Parse(hidSystemMoudleID.Text.Trim()));
+                obj.MoudleNameCn = this.txtMoudleNameCn.Text.Trim();
+                obj.MoudleNameEn = this.txtMoudleNameEn.Text.Trim();
+                obj.MoudleNameDb = this.txtMoudleNameDb.Text.Trim();
+                obj.MoudleDescription = this.txtMoudleDescription.Text.Trim();
+                //obj.ApplicationID = Convert.ToInt32(this.txtApplicationID.Text.Trim());
+                obj.MoudleIsSystemMoudle = this.chkMoudleIsSystemMoudle.Checked;
+
+                SystemMoudleWrapper.Update(obj);
+
+                winSystemMoudleEdit.Hide();
+                Coolite.Ext.Web.ScriptManager.AjaxSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                Coolite.Ext.Web.ScriptManager.AjaxSuccess = false;
+                Coolite.Ext.Web.ScriptManager.AjaxErrorMessage = "错误信息：" + ex.Message;
+                return;
+            }
+
+        }
     }
+
+
 }
