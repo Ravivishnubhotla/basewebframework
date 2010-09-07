bc48b8a5132457cc12265a25aeae78893be61016
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2010-09-07 07:34:31 +0000

    add sqlexport

---
+++ trunk/NewEXtjs/SPPipeManage/DataBaseSchemaExport/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+
+
+namespace DataBaseSchemaExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            string[] sqls = cfg.GenerateSchemaCreationScript(new MySQL5Dialect());
+
+            File.WriteAllLines("1.sql", sqls);
+
+            Console.WriteLine("OK");
+        }
+    }
+}
