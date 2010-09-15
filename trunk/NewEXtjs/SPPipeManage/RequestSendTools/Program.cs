0c360fba40a6f380d1405b03bcd2cf973b873d12
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2010-09-15 07:38:30 +0000

    add project

---
+++ trunk/NewEXtjs/SPPipeManage/RequestSendTools/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace RequestSendTools
+{
+    class Program
+    {
+        public static string ConnString = ConfigurationManager.ConnectionStrings["sqldb"].ConnectionString;
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static DataSet QueryData(string sql)
+        {
+            DataSet ds = new DataSet();
+            using(SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, ConnString))
+            {
+                sqlDataAdapter.Fill(ds);
+            }
+            return ds;
+        }
+    }
+}
