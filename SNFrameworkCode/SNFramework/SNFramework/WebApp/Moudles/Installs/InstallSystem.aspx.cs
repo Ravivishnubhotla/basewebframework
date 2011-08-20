792cd5c5c2cc09b6f025f2eee379739d5fdff8aa
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2011-08-20 08:27:55 +0000

    support sqllite and mysql(Need modify code)

--- SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/Installs/InstallSystem.aspx.cs
+++ SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/Installs/InstallSystem.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Legendigital.Common.WebApp.Moudles.Installs
 {
-    public partial class InstallDataBase : System.Web.UI.Page
+    public partial class InstallSystem : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
