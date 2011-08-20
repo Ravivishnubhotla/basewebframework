c940757222a7859a62e7ba447dd536b8fed87978
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2011-08-20 11:00:22 +0000

    fix code

--- trunk/SNFrameworkCode/SNFramework/SNFramework/Common/BaseFramework/Data/Tables/SystemShortMessageDataObject.Designer.cs
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/Common/BaseFramework/Data/Tables/SystemShortMessageDataObject.Designer.cs
@@ -18,11 +18,13 @@ namespace Legendigital.Framework.Common.BaseFramework.Data.Tables
 		public static readonly StringProperty PROPERTY_SHORTMESSAGETITLE = new StringProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGETITLE));		
 		public static readonly StringProperty PROPERTY_SHORTMESSAGECATEGORY = new StringProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGECATEGORY));		
 		public static readonly StringProperty PROPERTY_SHORTMESSAGECONTENT = new StringProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGECONTENT));		
-		public static readonly StringProperty PROPERTY_SHORTMESSAGESENDERNAMR = new StringProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGESENDERNAMR));		
+		public static readonly StringProperty PROPERTY_SHORTMESSAGESENDERNAME = new StringProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGESENDERNAME));		
+		public static readonly StringProperty PROPERTY_SHORTMESSAGETONAME = new StringProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGETONAME));		
 		public static readonly DateTimeProperty PROPERTY_SHORTMESSAGESENDDATE = new DateTimeProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGESENDDATE));		
 		public static readonly IntProperty PROPERTY_SHORTMESSAGESENDUSERID = new IntProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGESENDUSERID));		
 		public static readonly IntProperty PROPERTY_SHORTMESSAGERECEIVERUSERID = new IntProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGERECEIVERUSERID));		
 		public static readonly BoolProperty PROPERTY_SHORTMESSAGEISREAD = new BoolProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGEISREAD));		
+		public static readonly StringProperty PROPERTY_SHORTMESSAGETYPE = new StringProperty(Property.ForName(SystemShortMessageEntity.PROPERTY_NAME_SHORTMESSAGETYPE));		
       
 		#region 子类集合字段查询字段
 	
@@ -48,7 +50,9 @@ namespace Legendigital.Framework.Common.BaseFramework.Data.Tables
                     return typeof (string);
                 case "ShortMessageContent":
                     return typeof (string);
-                case "ShortMessageSenderNamr":
+                case "ShortMessageSenderName":
+                    return typeof (string);
+                case "ShortMessageToName":
                     return typeof (string);
                 case "ShortMessageSendDate":
                     return typeof (DateTime);
@@ -58,6 +62,8 @@ namespace Legendigital.Framework.Common.BaseFramework.Data.Tables
                     return typeof (int);
                 case "ShortMessageIsRead":
                     return typeof (bool);
+                case "ShortMessageType":
+                    return typeof (string);
           }
 			return typeof(string);
         }
