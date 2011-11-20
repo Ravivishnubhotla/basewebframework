af7b058719368337ab1e9cf7b0225169b757b283
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2011-11-20 08:58:31 +0000

    修正日志查询页面

--- trunk/SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/SystemManage/UserManage/SystemUserListPage.aspx.cs
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/SystemManage/UserManage/SystemUserListPage.aspx.cs
@@ -86,6 +86,12 @@ namespace Legendigital.Common.WebApp.Moudles.SystemManage.UserManage
             List<QueryFilter> queryFilters = new List<QueryFilter>();
 
             queryFilters.Add(new QueryFilter(SystemUserWrapper.PROPERTY_NAME_USERLOGINID, SystemUserWrapper.DEV_USER_ID,FilterFunction.NotEqualTo));
+            
+            if(!string.IsNullOrEmpty(this.txtSearchName.Text.Trim()))
+                queryFilters.Add(new QueryFilter(SystemUserWrapper.PROPERTY_NAME_USERLOGINID, this.txtSearchName.Text.Trim(), FilterFunction.Contains));
+
+
+
 
             storeSystemUser.DataSource = SystemUserWrapper.FindAllByOrderByAndFilter(queryFilters,sortFieldName, (e.Dir == Ext.Net.SortDirection.DESC), pageQueryParams);
             e.Total = recordCount;
