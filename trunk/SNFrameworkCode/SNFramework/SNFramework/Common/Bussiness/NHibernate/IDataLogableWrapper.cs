f05d0baf5534c616012f99326c5fe6abc8e9bd41
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2011-11-23 08:18:24 +0000

    change

---
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/Common/Bussiness/NHibernate/IDataLogableWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Legendigital.Framework.Common.Bussiness.NHibernate
+{
+    public interface IDataLogableWrapper
+    {
+          int? CreateBy { get; set; }
+
+          DateTime? CreateAt { get; set; }
+
+          int? LastModifyBy { get; set; }
+
+          DateTime? LastModifyAt { get; set; }
+
+          string LastModifyComment { get; set; }
+
+          void SetCreatByInfo(int createBy, DateTime createAt, string createComment);
+
+          void SetModifyByInfo(int lastModifyBy, DateTime lastModifyAt, string modifyComment);
+    }
+}
