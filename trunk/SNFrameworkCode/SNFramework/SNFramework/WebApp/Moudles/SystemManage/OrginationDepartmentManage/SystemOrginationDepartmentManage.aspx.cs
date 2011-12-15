329ed23418949071b7d83e7d52d24881b5c1fd32
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2011-12-15 14:10:21 +0000

    修改组织部门管理

--- trunk/SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/SystemManage/OrginationDepartmentManage/SystemOrginationDepartmentManage.aspx.cs
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/SystemManage/OrginationDepartmentManage/SystemOrginationDepartmentManage.aspx.cs
@@ -8,6 +8,7 @@ using Ext.Net;
 using Legendigital.Common.WebApp.AppCode;
 using Legendigital.Framework.Common.BaseFramework.Bussiness.Commons;
 using Legendigital.Framework.Common.BaseFramework.Bussiness.Wrappers;
+using Legendigital.Framework.Common.Data.NHibernate.DynamicQuery;
 using Legendigital.Framework.Common.Web.UI;
 using Legendigital.Framework.Common.BaseFramework.Web;
 
@@ -17,6 +18,69 @@ namespace Legendigital.Common.WebApp.Moudles.SystemManage.OrginationDepartmentMa
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+
+        protected void storeSystemPost_Refresh(object sender, StoreRefreshDataEventArgs e)
+        {
+
+            string sortFieldName = "";
+            if (e.Sort != null)
+                sortFieldName = e.Sort;
+
+            int startIndex = 0;
+
+            if (e.Start > -1)
+            {
+                startIndex = e.Start;
+            }
+
+            int limit = this.PagingToolBar1.PageSize;
+
+            int pageIndex = 1;
+
+            if ((startIndex % limit) == 0)
+                pageIndex = startIndex / limit + 1;
+            else
+                pageIndex = startIndex / limit;
+
+
+            int selectOrgID = 0;
+
+            if (e.Parameters["SelectOrgID"] != null)
+            {
+                selectOrgID = Convert.ToInt32(e.Parameters["SelectOrgID"]);
+            }
+
+            if (selectOrgID == 0)
+            {
+                storeSystemPost.DataSource = new List<SystemPostWrapper>();
+                e.Total = 0;
+                storeSystemPost.DataBind();
+
+                return;
+            }
+
+            SystemOrganizationWrapper organization = SystemOrganizationWrapper.FindById(selectOrgID);
+
+            if (organization == null)
+            {
+                storeSystemPost.DataSource = new List<SystemPostWrapper>();
+                e.Total = 0;
+                storeSystemPost.DataBind();
+
+                return;
+            }
+
+            PageQueryParams pageQueryParams = new PageQueryParams();
+            pageQueryParams.PageSize = limit;
+            pageQueryParams.PageIndex = pageIndex;
+
+            storeSystemPost.DataSource = SystemPostWrapper.FindAllByOrderByAndFilterAndOrganizationID(sortFieldName, (e.Dir == Ext.Net.SortDirection.DESC), organization, pageQueryParams);
+            e.Total = pageQueryParams.RecordCount;
+
+            storeSystemPost.DataBind();
 
         }
 
---
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/SystemManage/OrginationDepartmentManage/UCSystemPostEdit.ascx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ext.Net;
+using Legendigital.Framework.Common.BaseFramework.Bussiness.Wrappers;
+using Legendigital.Framework.Common.BaseFramework.Web;
+using Legendigital.Framework.Common.Utility;
+
+namespace Legendigital.Common.WebApp.Moudles.SystemManage.OrginationDepartmentManage
+{
+    [DirectMethodProxyID(IDMode = DirectMethodProxyIDMode.Alias, Alias = "UCSystemPostEdit")]
+    public partial class UCSystemPostEdit : BaseUserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        [DirectMethod]
+        public void Show(int id)
+        {
+            try
+            {
+                SystemPostWrapper obj = SystemPostWrapper.FindById(id);
+
+                if (obj != null)
+                {
+                    this.txtName.Text = ValueConvertUtil.ConvertStringValue(obj.Name);
+                    this.txtCode.Text = ValueConvertUtil.ConvertStringValue(obj.Code);
+                    this.txtDescription.Text = ValueConvertUtil.ConvertStringValue(obj.Description);
+                    this.txtOrganizationID.Text = obj.OrganizationID.Name;
+ 
+
+                    hidId.Text = id.ToString();
+
+
+                    winSystemPostEdit.Show();
+
+                }
+                else
+                {
+                    ResourceManager.AjaxSuccess = false;
+                    ResourceManager.AjaxErrorMessage = "Error Message : Data does not exist";
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                ResourceManager.AjaxSuccess = false;
+                ResourceManager.AjaxErrorMessage = "Error Message : " + ex.Message;
+                return;
+            }
+        }
+
+        protected void btnSaveSystemPost_Click(object sender, DirectEventArgs e)
+        {
+            try
+            {
+                SystemPostWrapper obj = SystemPostWrapper.FindById(int.Parse(hidId.Text.Trim()));
+                obj.Name = this.txtName.Text.Trim();
+                obj.Code = this.txtCode.Text.Trim();
+                obj.Description = this.txtDescription.Text.Trim();
+
+
+                obj.LastModifyBy = this.ParentPage.CurrentLoginUser.UserID;
+                obj.LastModifyAt = System.DateTime.Now; ;
+                obj.LastModifyComment = "创建用户";
+
+
+                SystemPostWrapper.Update(obj);
+
+                winSystemPostEdit.Hide();
+                ResourceManager.AjaxSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                ResourceManager.AjaxSuccess = false;
+                ResourceManager.AjaxErrorMessage = "Error Message:" + ex.Message;
+                return;
+            }
+
+        }
+
+    }
+}
