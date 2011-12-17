9485dc2c67eec401eb19baa33694a2f42ffa39e8
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2011-12-17 07:15:15 +0000

    SP模块完善

---
+++ SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/SPS/Codes/SPSChannelCodeHandler.ashx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Legendigital.Common.WebApp.Moudles.SPS.Codes
+{
+    /// <summary>
+    /// Summary description for SPSChannelCodeHandler
+    /// </summary>
+    public class SPSChannelCodeHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (!string.IsNullOrEmpty(context.Request.QueryString["ClientID"]))
+            {
+
+            }
+
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
