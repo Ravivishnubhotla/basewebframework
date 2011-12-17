e06dd0fb261c8fbd4b20af6ef6f5d7667fca3f5e
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2011-12-17 18:20:18 +0000

    完善短信模块

---
+++ SNFrameworkCode/SNFramework/SNFramework/WebApp/Moudles/SPS/Channels/SPChannelSendTestRequestForm.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ext.Net;
+using Legendigital.Framework.Common.BaseFramework.Bussiness.SystemConst;
+using Legendigital.Framework.Common.Utility;
+using SPS.Bussiness.Code;
+using SPS.Bussiness.Wrappers;
+
+namespace Legendigital.Common.WebApp.Moudles.SPS.Channels
+{
+    public partial class SPChannelSendTestRequestForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (X.IsAjaxRequest)
+                return;
+
+ 
+
+            SPChannelWrapper channelWrapper = ChannelID;
+
+            this.hidChannelID.Text = channelWrapper.Id.ToString();
+
+            this.txtChannelName.Text = channelWrapper.Name;
+
+            this.txtChannelSubmitUrl.Text = channelWrapper.InterfaceUrl;
+
+
+            SPChannelParamsCollection channelParamsWrappers = channelWrapper.ChannelParams;
+
+            foreach (SPChannelParamsWrapper spChannelParamsWrapper in channelParamsWrappers.Items)
+            {
+ 
+                TextField txt = new TextField();
+                txt.ID = "txt" + spChannelParamsWrapper.Name;
+                txt.FieldLabel = spChannelParamsWrapper.Name;
+
+                if (spChannelParamsWrapper.ParamsMappingName == DictionaryConst.Dictionary_SPField_LinkID_Key)
+                {
+                    txt.Value = "test" + StringUtil.GetRandNumber(10);
+
+                    hidLinkIDeName.Text = txt.ClientID;
+                }
+
+                if (spChannelParamsWrapper.ParamsMappingName == DictionaryConst.Dictionary_SPField_Mobile_Key)
+                {
+                    txt.Value = "135" + StringUtil.GetRandNumber(8);
+
+                    hidMobileName.Text = txt.ClientID;
+                }
+
+                txt.AnchorHorizontal = "95%";
+                this.FormPanel1.Items.Add(txt);
+            }
+
+        }
+
+
+
+        public SPChannelWrapper ChannelID
+        {
+            get
+            {
+                if (this.Request.QueryString["ChannelID"] != null)
+                {
+                    return
+                        SPChannelWrapper.FindById(int.Parse(this.Request.QueryString["ChannelID"]));
+                }
+                return null;
+            }
+        }
+
+
+
+
+        [DirectMethod]
+        public SPMessage GetTestSPMessage()
+        {
+            SPMessage spMessage = new SPMessage();
+            spMessage.LinkID = "test" + StringUtil.GetRandNumber(10);
+            spMessage.Mobile = "135" + StringUtil.GetRandNumber(8);
+            spMessage.Mo = "";
+            spMessage.SPCode = "";
+            return spMessage;
+        }
+    }
+}
