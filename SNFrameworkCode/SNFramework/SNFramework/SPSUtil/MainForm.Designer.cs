a98883c425568000dde73026adc30911a494d124
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-01-13 14:30:47 +0000

    新建SPSwinform项目

---
+++ SNFrameworkCode/SNFramework/SNFramework/SPSUtil/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SPSUtil
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.msTop = new System.Windows.Forms.MenuStrip();
+            this.ssBottom = new System.Windows.Forms.StatusStrip();
+            this.tsTop = new System.Windows.Forms.ToolStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.scMain = new System.Windows.Forms.SplitContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.tvLeft = new System.Windows.Forms.TreeView();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.tsTop.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scMain)).BeginInit();
+            this.scMain.Panel1.SuspendLayout();
+            this.scMain.SuspendLayout();
+            this.pnlLeft.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msTop
+            // 
+            this.msTop.Location = new System.Drawing.Point(0, 0);
+            this.msTop.Name = "msTop";
+            this.msTop.Size = new System.Drawing.Size(890, 24);
+            this.msTop.TabIndex = 0;
+            this.msTop.Text = "menuStrip1";
+            // 
+            // ssBottom
+            // 
+            this.ssBottom.Location = new System.Drawing.Point(0, 453);
+            this.ssBottom.Name = "ssBottom";
+            this.ssBottom.Size = new System.Drawing.Size(890, 22);
+            this.ssBottom.TabIndex = 1;
+            this.ssBottom.Text = "statusStrip1";
+            // 
+            // tsTop
+            // 
+            this.tsTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.tsTop.Location = new System.Drawing.Point(0, 24);
+            this.tsTop.Name = "tsTop";
+            this.tsTop.Size = new System.Drawing.Size(890, 25);
+            this.tsTop.TabIndex = 2;
+            this.tsTop.Text = "toolStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.scMain);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(890, 404);
+            this.panel1.TabIndex = 3;
+            // 
+            // scMain
+            // 
+            this.scMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scMain.Location = new System.Drawing.Point(0, 0);
+            this.scMain.Name = "scMain";
+            // 
+            // scMain.Panel1
+            // 
+            this.scMain.Panel1.Controls.Add(this.pnlLeft);
+            this.scMain.Panel1.Controls.Add(this.toolStrip1);
+            this.scMain.Size = new System.Drawing.Size(890, 404);
+            this.scMain.SplitterDistance = 155;
+            this.scMain.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(155, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.Controls.Add(this.tvLeft);
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 25);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(155, 379);
+            this.pnlLeft.TabIndex = 1;
+            // 
+            // tvLeft
+            // 
+            this.tvLeft.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvLeft.Location = new System.Drawing.Point(0, 0);
+            this.tvLeft.Name = "tvLeft";
+            this.tvLeft.Size = new System.Drawing.Size(155, 379);
+            this.tvLeft.TabIndex = 0;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(101, 22);
+            this.toolStripButton1.Text = "Http请求测试";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 475);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tsTop);
+            this.Controls.Add(this.ssBottom);
+            this.Controls.Add(this.msTop);
+            this.MainMenuStrip = this.msTop;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.tsTop.ResumeLayout(false);
+            this.tsTop.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.scMain.Panel1.ResumeLayout(false);
+            this.scMain.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scMain)).EndInit();
+            this.scMain.ResumeLayout(false);
+            this.pnlLeft.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msTop;
+        private System.Windows.Forms.StatusStrip ssBottom;
+        private System.Windows.Forms.ToolStrip tsTop;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer scMain;
+        private System.Windows.Forms.Panel pnlLeft;
+        private System.Windows.Forms.TreeView tvLeft;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
