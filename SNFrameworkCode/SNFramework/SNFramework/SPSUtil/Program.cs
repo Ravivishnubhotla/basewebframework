de6d8114dc21e18628f74f339255c5651dc93794
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-01-13 23:26:24 +0000

    add subsonic

--- SNFrameworkCode/SNFramework/SNFramework/SPSUtil/Program.cs
+++ SNFrameworkCode/SNFramework/SNFramework/SPSUtil/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using SubSonic.Repository;
 
 namespace SPSUtil
 {
@@ -13,6 +14,7 @@ namespace SPSUtil
         [STAThread]
         static void Main()
         {
+            var repo = new SimpleRepository("NorthwindSQLite", SimpleRepositoryOptions.RunMigrations);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
