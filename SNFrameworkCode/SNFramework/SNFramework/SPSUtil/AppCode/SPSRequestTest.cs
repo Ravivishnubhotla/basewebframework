b25310edf6809326af4a7da9ba1b2db235c37285
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-01-14 01:59:25 +0000

    1

---
+++ SNFrameworkCode/SNFramework/SNFramework/SPSUtil/AppCode/SPSRequestTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPSUtil.AppCode
+{
+    public enum SPSRequestType
+    {
+        HttpGet,
+        HttpPost,
+        HttpXMLPost
+    }
+
+    [Serializable]
+    public class SPSRequestTest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public SPSRequestType RequestType { get; set; }
+
+
+    }
+}
