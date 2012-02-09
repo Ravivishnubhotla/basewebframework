10d56165ed2200c9043eab739a5dab25a5e55866
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-02-09 02:21:55 +0000

    1

--- trunk/SNFrameworkCode/SNFramework/SNFramework/SPSUtil/Moudles/frmDataSendTask.Designer.cs
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/SPSUtil/Moudles/frmDataSendTask.Designer.cs
@@ -28,19 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDataSendTask));
             this.pnlMain = new System.Windows.Forms.Panel();
-            this.tsTop = new System.Windows.Forms.ToolStrip();
-            this.ssBottom = new System.Windows.Forms.StatusStrip();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tsTop = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.ssBottom = new System.Windows.Forms.StatusStrip();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.pnlMain.SuspendLayout();
-            this.tsTop.SuspendLayout();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tsTop.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlMain
@@ -52,25 +59,6 @@
             this.pnlMain.Size = new System.Drawing.Size(880, 421);
             this.pnlMain.TabIndex = 11;
             // 
-            // tsTop
-            // 
-            this.tsTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2});
-            this.tsTop.Location = new System.Drawing.Point(0, 0);
-            this.tsTop.Name = "tsTop";
-            this.tsTop.Size = new System.Drawing.Size(880, 25);
-            this.tsTop.TabIndex = 10;
-            this.tsTop.Text = "toolStrip1";
-            // 
-            // ssBottom
-            // 
-            this.ssBottom.Location = new System.Drawing.Point(0, 446);
-            this.ssBottom.Name = "ssBottom";
-            this.ssBottom.Size = new System.Drawing.Size(880, 22);
-            this.ssBottom.TabIndex = 9;
-            this.ssBottom.Text = "statusStrip1";
-            // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -81,8 +69,12 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
             this.splitContainer1.Size = new System.Drawing.Size(880, 421);
-            this.splitContainer1.SplitterDistance = 303;
+            this.splitContainer1.SplitterDistance = 352;
             this.splitContainer1.TabIndex = 0;
             // 
             // dataGridView1
@@ -91,26 +83,98 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(880, 303);
+            this.dataGridView1.Size = new System.Drawing.Size(880, 352);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // tsTop
+            // 
+            this.tsTop.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.tsTop.Location = new System.Drawing.Point(0, 0);
+            this.tsTop.Name = "tsTop";
+            this.tsTop.Size = new System.Drawing.Size(880, 25);
+            this.tsTop.TabIndex = 10;
+            this.tsTop.Text = "toolStrip1";
             // 
             // toolStripButton1
             // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.Image = global::SPSUtil.Properties.Resources.page_excel;
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(77, 22);
+            this.toolStripButton1.Text = "加载Excel";
             // 
             // toolStripButton2
             // 
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.Image = global::SPSUtil.Properties.Resources.page_white_text;
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(73, 22);
+            this.toolStripButton2.Text = "加载Text";
+            // 
+            // ssBottom
+            // 
+            this.ssBottom.Location = new System.Drawing.Point(0, 446);
+            this.ssBottom.Name = "ssBottom";
+            this.ssBottom.Size = new System.Drawing.Size(880, 22);
+            this.ssBottom.TabIndex = 9;
+            this.ssBottom.Text = "statusStrip1";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = global::SPSUtil.Properties.Resources.page_edit;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(99, 22);
+            this.toolStripButton3.Text = "数据加载配置";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(880, 65);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "发送Url模版 ： ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(777, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(683, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "生成测试链接";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(793, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "发送数据";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // frmDataSendTask
             // 
@@ -121,13 +185,16 @@
             this.Controls.Add(this.tsTop);
             this.Controls.Add(this.ssBottom);
             this.Name = "frmDataSendTask";
-            this.Text = "frmDataSendTask";
+            this.Text = "批量发送数据";
             this.pnlMain.ResumeLayout(false);
-            this.tsTop.ResumeLayout(false);
-            this.tsTop.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tsTop.ResumeLayout(false);
+            this.tsTop.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,5 +209,11 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
