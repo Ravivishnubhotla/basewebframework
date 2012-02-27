ef59752c509fd9e57ecda955dda0692b44be0057
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-02-27 04:05:41 +0000

    1

---
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/Common/Utility/EncodeUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Legendigital.Framework.Common.Utility
+{
+    public static class EncodeUtil
+    {
+        public static readonly HashSet<string> JscriptSpecicalCharacters;
+
+        static EncodeUtil()
+        {
+            //string[] JscriptSpecicalCharacterArrays = ("")
+
+            //HashSet<string> jssc = new HashSet<string>();
+            //jssc.Add("");
+
+            //JscriptSpecicalCharacters = new HashSet<string>();
+        }
+
+        public static string JscriptEncode(string encodeString)
+        {
+            return "";
+        }
+    }
+}
