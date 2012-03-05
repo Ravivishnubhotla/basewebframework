53341105659b8eab981eba693bbf4ad6d7373bc9
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-03-05 08:17:51 +0000

    AOP研究

---
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/Common/BaseFramework/Bussiness/ServiceProxys/Aop/RecordAuditorMethodAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Legendigital.Framework.Common.BaseFramework.Bussiness.ServiceProxys.Aop
+{
+    public delegate void RecordAuditorHandle(int createUserID, DateTime createTime,string relateMoudleName ,int relateMoudleID);
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public class RecordAuditorMethodAttribute : Attribute
+    {
+        public RecordAuditorHandle AuditorActionHandle { get; set; }
+    }
+}
--- trunk/SNFrameworkCode/SNFramework/SNFramework/Common/BaseFramework/Bussiness/ServiceProxys/Aop/ServiceProxyAroundAdvice.cs
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/Common/BaseFramework/Bussiness/ServiceProxys/Aop/ServiceProxyAroundAdvice.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 using AopAlliance.Intercept;
 using Common.Logging;
+using Legendigital.Framework.Common.BaseFramework.Bussiness.Wrappers;
+using Legendigital.Framework.Common.BaseFramework.Web;
+using Legendigital.Framework.Common.Entity;
 
 namespace Legendigital.Framework.Common.BaseFramework.Bussiness.ServiceProxys.Aop
 {
@@ -11,26 +15,83 @@ namespace Legendigital.Framework.Common.BaseFramework.Bussiness.ServiceProxys.Ao
     {
         private static ILog logger = LogManager.GetLogger(typeof(ServiceProxyAroundAdvice));
 
+        private static Dictionary<Type, RecordAuditorClassAttribute> recordAuditors = new Dictionary<Type, RecordAuditorClassAttribute>();
+
+        public RecordAuditorClassAttribute GetRecordAuditorAttribute(Type type)
+        {
+            if (recordAuditors.ContainsKey(type))
+            {
+                return recordAuditors[type];
+            }
+
+            var attr = type.GetCustomAttributes(typeof(RecordAuditorClassAttribute), false).FirstOrDefault() as RecordAuditorClassAttribute;
+
+            if (attr != null)
+            {
+                recordAuditors[type] = attr;
+            }
+            else
+            {
+                recordAuditors[type] = new RecordAuditorClassAttribute() { EnableVersion = false };
+            }
+
+
+            return recordAuditors[type];
+        }
+
         public object Invoke(IMethodInvocation invocation)
         {
-            bool needAuditor = false;
-
-            var attr = invocation.Method.GetCustomAttributes(typeof(RecordAuditorLogAttribute), false).FirstOrDefault() as RecordAuditorLogAttribute;
-
-            needAuditor = (attr != null);
+            RecordAuditorClassAttribute auditorClassAttribute = GetRecordAuditorAttribute(invocation.TargetType);
 
             logger.Info("开始:  " + invocation.TargetType.Name + "." + invocation.Method.Name);
-            
-            object result = invocation.Proceed();
 
-            if (needAuditor)
+ 
+            DateTime operateTime = System.DateTime.Now;
+
+            int operateUserID = SystemUserWrapper.GetCurrentOperateUserID();
+
+            switch (invocation.Method.Name)
             {
-
+                case "Save":
+                    SetCreateInfo(invocation.Arguments[0], operateTime, operateUserID, invocation.Method.Name);
+                    break;
+                case "Update":
+                    SetUpdateInfo(invocation.Arguments[0], operateTime, operateUserID, invocation.Method.Name);
+                    break;
+                case "SaveOrUpdate":
+                    break;
             }
+     
+
+            object result = invocation.Proceed();
 
             logger.Info("结束:  " + invocation.TargetType.Name + "." + invocation.Method.Name);
             
             return result;
+        }
+
+        private void SetCreateInfo(object saveEntity, DateTime operateTime, int operateUserId, string opName)
+        {
+            IAuditableEntity iSaveEntity = saveEntity as IAuditableEntity;
+
+            if (iSaveEntity != null)
+            {
+                iSaveEntity.CreateAt = operateTime;
+                iSaveEntity.CreateBy = operateUserId;
+                iSaveEntity.LastModifyComment = opName;
+            }
+        }
+
+        private void SetUpdateInfo(object updateEntity, DateTime operateTime, int operateUserId, string opName)
+        {
+            IAuditableEntity iUpdateEntity = updateEntity as IAuditableEntity;
+
+            if (iUpdateEntity != null)
+            {
+                iUpdateEntity.LastModifyAt = operateTime;
+                iUpdateEntity.LastModifyBy = operateUserId;
+                iUpdateEntity.LastModifyComment = opName;
+            }
         }
     }
 }
