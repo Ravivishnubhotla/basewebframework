e54359e50f1746ef3c736874f32eb54c8abb07bf
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-04-25 12:53:40 +0000

    实现JqueryUi界面

---
+++ HotSpot/SLHotSpot/SLHotSpot.Web/Handler1.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SLHotSpot.Web
+{
+    /// <summary>
+    /// Summary description for Handler1
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(File.ReadAllText(context.Server.MapPath("~/data/datagrid_data.json")));
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
