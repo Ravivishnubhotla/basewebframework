0aee9d695f0fd64a9562938a669ce6fbba944ff1
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-04-28 03:45:26 +0000

    1

---
+++ HotSpot/SLHotSpot/SLHotSpot.Web/ShopMallLayoutData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLHotSpot.Web
+{
+    public class ShopMallLayoutData
+    {
+        public string ShopMallNo { get; set; }
+        public string ShopMallFloorNo { get; set; }
+        public List<RosShopInfo> ShopInfos { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageWith { get; set; }
+        public string ImageHeight { get; set; }
+    }
+}
