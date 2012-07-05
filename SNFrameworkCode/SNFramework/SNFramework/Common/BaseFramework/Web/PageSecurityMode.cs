47338463f30c0a3e6944a9729c8f00a200379716
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-07-05 00:47:26 +0000

    修改数据补发工具

---
+++ SNFrameworkCode/SNFramework/SNFramework/Common/BaseFramework/Web/PageSecurityMode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Legendigital.Framework.Common.BaseFramework.Web
+{
+    /// <summary>
+    /// 页面验证模式
+    /// </summary>
+    public enum PageSecurityMode
+    {
+        /// <summary>
+        /// 不需要
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// 需要验证
+        /// </summary>
+        Authentication = 1
+    }
+}
---
+++ SNFrameworkCode/SNFramework/SNFramework/Common/Utility/ExcelFileReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+
+namespace Legendigital.Framework.Common.Utility
+{
+    public static class ExcelFileReader
+    {
+        public static DataTable ReadExcelSheet(string sheetName, string excelPath)
+        {
+            OleDbConnection conn = new OleDbConnection();
+
+            conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + excelPath + "; Extended Properties='Excel 8.0;HDR=Yes;IMEX=1'";
+ 
+            conn.Open();
+
+            string sql = string.Format("select * from [{0}$]", sheetName);
+ 
+            OleDbCommand olecommand = new OleDbCommand(sql, conn);
+
+            DataSet ds = new DataSet();
+
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(olecommand);
+
+            dataAdapter.Fill(ds);
+ 
+            conn.Close();
+
+            return ds.Tables[0];
+        }
+    }
+}
