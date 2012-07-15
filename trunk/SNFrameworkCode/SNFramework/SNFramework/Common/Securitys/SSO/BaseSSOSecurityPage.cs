c8a77abe23fa2ef05050d1c663297cb77d384812
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-07-15 12:02:27 +0000

    Update ext.net to 1.5.1

---
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/Common/Securitys/SSO/BaseSSOSecurityPage.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+using Legendigital.Framework.Common.BaseFramework.Bussiness.Wrappers;
+using Legendigital.Framework.Common.BaseFramework.Web;
+
+namespace Legendigital.Framework.Common.Securitys.SSO
+{
+    public class BaseSSOSecurityPage : System.Web.UI.Page
+    {
+   
+        public SystemUserWrapper CurrentLoginUser
+        {
+            get
+            {
+                if (Context.User == null)
+                {
+                    Session[SSOProvider.Session_Key_LoginUser] = null;
+                    return null;
+                }
+                if (Session[SSOProvider.Session_Key_LoginUser] == null)
+                {
+                    Session[SSOProvider.Session_Key_LoginUser] =
+                        SystemUserWrapper.GetInitalUserByLoginID(Context.User.Identity.Name);
+                }
+                return (SystemUserWrapper)Session[SSOProvider.Session_Key_LoginUser];
+            }
+            set { Session[SSOProvider.Session_Key_LoginUser] = value; }
+        }
+
+        public BaseSecurityPage CurrentSecurityPage
+        {
+            get
+            {
+                BaseSecurityPage page = this.Page as BaseSecurityPage;
+
+                return page;
+            }
+        }
+
+        protected SecurityMode pageSecurityMode = SecurityMode.Authentication;
+        public SystemUserWrapper currentLoginUser = null;
+
+        //页面初始化
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            if (pageSecurityMode == SecurityMode.Authentication)
+            {
+                Authentication();
+            }
+        }
+
+        /// <summary>
+        /// 验证用户身份
+        /// </summary>
+        private void Authentication()
+        {
+ 
+
+        }
+
+        private void GetLoginUserInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RedirectToLogon(string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public void RefreshUser()
+        //{
+        //    Session[SSOProvider.Session_Key_LoginUser] =
+        //                SystemUserWrapper.GetInitalUserByLoginID(Context.User.Identity.Name);
+        //}
+
+        //public void ClearLoginInfo()
+        //{
+        //    //if (CurrentLoginUser!=null)
+        //    //    SystemLogWrapper.LogUserLoginOutSuccessed(CurrentLoginUser, HttpUtil.GetIP(this.Request), System.DateTime.Now);
+        //    Session[SSOProvider.Session_Key_LoginUser] = null;
+        //    FormsAuthentication.SignOut();
+        //}
+
+        //public string[] CurrentLoginUserAssignedRole
+        //{
+        //    get
+        //    {
+        //        return SystemUserWrapper.GetRolesForUser(Context.User.Identity.Name);
+        //    }
+        //}
+
+        //public void CurrentLoginUserSignOut()
+        //{
+        //    Session[SSOProvider.Session_Key_LoginUser] = null;
+        //    FormsAuthentication.SignOut();
+        //    FormsAuthentication.RedirectToLoginPage();
+        //}
+
+        //public bool CurrentUserHasPermission(string permissionCode)
+        //{
+        //    return SystemUserWrapper.UserHasPermission(this.CurrentLoginUser, permissionCode);
+        //}
+
+        //public bool CurrentLoginUserIsDevelopeAdmin()
+        //{
+        //    if (CurrentLoginUser == null)
+        //        return false;
+        //    if (CurrentLoginUser.UserLoginID == SystemUserWrapper.DEV_USER_ID)
+        //    {
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
+
+
+
+
+
+
+        //protected override void OnLoad(EventArgs e)
+        //{
+        //    //处理跨域的问题
+        //    Response.AddHeader("P3P", "CP=CAO PSA OUR");
+
+        //    if (SecurityMode == Enum.SSFEnum.SecurityMode.Normal)
+        //    {
+        //        AUTH();
+        //    }
+        //    else
+        //    {
+        //        GetLoginUserInfo();
+        //    }
+        //    base.OnLoad(e);
+        //}
+    }
+}
---
+++ trunk/SNFrameworkCode/SNFramework/SNFramework/Common/Securitys/SSO/SSOProvider.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Legendigital.Framework.Common.Utility;
+
+namespace Legendigital.Framework.Common.Securitys.SSO
+{
+    /// <summary>
+    /// 登陆类型（指常规登陆还是key登陆，手机登陆）
+    /// </summary>
+    public enum LoginType
+    {
+        /// <summary>
+        /// 普通登陆
+        /// </summary>
+        Normal = 1,
+        /// <summary>
+        /// 通过Key登陆
+        /// </summary>
+        UseKey = 2
+    }
+    /// <summary>
+    /// 子页面验证方式
+    /// </summary>
+    public enum SecurityMode
+    {
+        /// <summary>
+        /// 需要验证
+        /// </summary>
+        Authentication = 1,
+        /// <summary>
+        /// 不需要验证
+        /// </summary>
+        None = 2
+    }
+
+    public class SSOProvider
+    {
+        public const string NODE_NAME_SSFToken = "SSFToken";
+        public const string ELEMENT_NAME_LoginUserID = "LoginUserID";
+        public const string ELEMENT_NAME_Password = "Password";
+        public const string ELEMENT_NAME_LoginDate = "LoginDate";
+        public const string ELEMENT_NAME_IPAddress = "IPAddress";
+        public const string ELEMENT_NAME_SSOKey = "SSOKey";
+        public const string QUERY_STRING_NAME_SSFToken = "_SSFToken";
+        public const string Session_Key_LoginUser = "CurrentLoginUser";
+ 
+        public static string GetSSFToken(string loginUserID, string password, DateTime loginDate, string ipAddress, string ssoKey, string SSFTokenKey)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(docNode);
+
+            XmlNode SSFTokenNode = doc.CreateElement(NODE_NAME_SSFToken);
+            doc.AppendChild(SSFTokenNode);
+
+            XmlAttribute UsernameAttribute = doc.CreateAttribute(ELEMENT_NAME_LoginUserID);
+            UsernameAttribute.Value = loginUserID;
+            SSFTokenNode.Attributes.Append(UsernameAttribute);
+
+            XmlAttribute PasswordAttribute = doc.CreateAttribute(ELEMENT_NAME_Password);
+            PasswordAttribute.Value = password;
+            SSFTokenNode.Attributes.Append(PasswordAttribute);
+
+            XmlAttribute DateAttribute = doc.CreateAttribute(ELEMENT_NAME_LoginDate);
+            DateAttribute.Value = loginDate.ToString("yyyy-MM-dd HH:mm:ss");
+            SSFTokenNode.Attributes.Append(DateAttribute);
+
+            XmlAttribute ipAddressAttribute = doc.CreateAttribute(ELEMENT_NAME_IPAddress);
+            ipAddressAttribute.Value = ipAddress;
+            SSFTokenNode.Attributes.Append(ipAddressAttribute);
+
+            XmlAttribute SSOKeyAttribute = doc.CreateAttribute(ELEMENT_NAME_SSOKey);
+            SSOKeyAttribute.Value = ssoKey;
+            SSFTokenNode.Attributes.Append(SSOKeyAttribute);
+
+            StringWriter sw = new StringWriter();
+            doc.Save(sw);
+
+            return CryptographyUtil.EncryptDES(sw.ToString(), SSFTokenKey);
+
+        }
+ 
+        /// <summary>
+        /// 返回从Token中出来的用户名和密码
+        /// </summary>
+        /// <param name="SSFToken"></param>
+        /// <returns></returns>
+        internal static Hashtable GetUserNameAndPasswordFromSSFToken(string SSFToken,string SSFTokenKey)
+        {
+            Hashtable SSFTokenUserNameAndPassword = new Hashtable();
+            try
+            {
+                XmlDocument xdc = new XmlDocument();
+                xdc.LoadXml(CryptographyUtil.DecryptDES(SSFToken, SSFTokenKey));
+                SSFTokenUserNameAndPassword.Add(ELEMENT_NAME_LoginUserID, xdc.DocumentElement.GetAttribute(ELEMENT_NAME_LoginUserID).ToString());
+                SSFTokenUserNameAndPassword.Add(ELEMENT_NAME_Password, xdc.DocumentElement.GetAttribute(ELEMENT_NAME_Password).ToString());
+                SSFTokenUserNameAndPassword.Add(ELEMENT_NAME_LoginDate, xdc.DocumentElement.GetAttribute(ELEMENT_NAME_LoginDate).ToString());
+                SSFTokenUserNameAndPassword.Add(ELEMENT_NAME_IPAddress, xdc.DocumentElement.GetAttribute(ELEMENT_NAME_IPAddress).ToString());
+                SSFTokenUserNameAndPassword.Add(ELEMENT_NAME_SSOKey, xdc.DocumentElement.GetAttribute(ELEMENT_NAME_SSOKey).ToString());
+                return SSFTokenUserNameAndPassword;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+ 
+ 
+        /// <summary>
+        /// 生成登录的SSOKey，只在登录时调用一次
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateSSOKey(string userLoginID)
+        {
+            string SSOKey = Guid.NewGuid().ToString();
+            //UserInfo.UpdateSSOKey(userLoginID, SSOKey);
+            return SSOKey;
+        }
+
+
+ 
+ 
+ 
+    }
+}
