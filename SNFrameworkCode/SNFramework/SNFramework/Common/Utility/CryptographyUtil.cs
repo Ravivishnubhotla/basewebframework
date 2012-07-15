5aae3a393f0355c2fb1a9a5f28823933c4c5b6d8
kiler398 <kiler398@0d1b7a07-4249-0410-bb35-eb5a4b088611>
2012-07-15 12:02:27 +0000

    Update ext.net to 1.5.1

--- SNFrameworkCode/SNFramework/SNFramework/Common/Utility/CryptographyUtil.cs
+++ SNFrameworkCode/SNFramework/SNFramework/Common/Utility/CryptographyUtil.cs
@@ -30,7 +30,6 @@ namespace Legendigital.Framework.Common.Utility
 
         //默认密钥向量 
         private static byte[] Keys = { 0x12, 0x34, 0x56, 0x98, 0xAA, 0xAB, 0xCD, 0xEF };
-        private static string encryptKey = "IGLREQUEST";
         /**/
         /**/
         /**/
@@ -40,8 +39,7 @@ namespace Legendigital.Framework.Common.Utility
         /// <param name="encryptString">待加密的字符串</param> 
         /// <param name="encryptKey">加密密钥,要求为8位</param> 
         /// <returns>加密成功返回加密后的字符串，失败返回源串</returns> 
-        //    public static string EncryptDES(string encryptString, string encryptKey)
-        public static string EncryptDES(string encryptString)
+        public static string EncryptDES(string encryptString, string encryptKey)
         {
             try
             {
@@ -70,7 +68,7 @@ namespace Legendigital.Framework.Common.Utility
         /// <param name="decryptString">待解密的字符串</param> 
         /// <param name="decryptKey">解密密钥,要求为8位,和加密密钥相同</param> 
         /// <returns>解密成功返回解密后的字符串，失败返源串</returns> 
-        public static string DecryptDES(string decryptString)
+        public static string DecryptDES(string decryptString, string encryptKey)
         {
             try
             {
--- SNFrameworkCode/SNFramework/SNFramework/WebApp/TestPage/TestFlash.aspx.cs
+++ SNFrameworkCode/SNFramework/SNFramework/WebApp/TestPage/TestFlash.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Legendigital.Framework.Common.BaseFramework.Bussiness.Wrappers;
 
 namespace Legendigital.Common.WebApp.TestPage
 {
     public partial class TestFlash : System.Web.UI.Page
     {
+ 
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
