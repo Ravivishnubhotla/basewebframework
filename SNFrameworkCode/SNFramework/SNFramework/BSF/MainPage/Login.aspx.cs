9b4c7df6bb1fc7c8dfcc6c1890c8c4053373d400
kiler398 <kiler398@gmail.com>
2012-10-21 14:39:04 +0000

    修改用户登陆日志

--- SNFrameworkCode/SNFramework/SNFramework/BSF/MainPage/Login.aspx.cs
+++ SNFrameworkCode/SNFramework/SNFramework/BSF/MainPage/Login.aspx.cs
@@ -126,7 +126,15 @@ namespace SNFramework.BSF.MainPage
             ssoTokenInfo.SSOKey = ssoKey;
             ssoTokenInfo.Email = userWrapper.UserEmail;
 
-            SystemLogWrapper.LogUserLoginSuccessed(userWrapper, ipaddress, loginDate);
+            userWrapper.LastLoginDate = loginDate;
+            userWrapper.LastLoginIP = ipaddress;
+            userWrapper.LastModifyAt = loginDate;
+            userWrapper.LastModifyBy = userWrapper.UserID;
+            userWrapper.LastModifyComment = "用户登陆更新信息";
+
+            SystemUserWrapper.Update(userWrapper);
+
+            SystemLogWrapper.LogUserLoginSuccessed(userWrapper);
  
             string token = SSOProvider.GetSSFToken(ssoTokenInfo);
 
