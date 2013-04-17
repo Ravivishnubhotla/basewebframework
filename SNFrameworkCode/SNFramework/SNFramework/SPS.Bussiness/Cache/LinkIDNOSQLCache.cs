d98722ae0d7de0f53573bd65ae284446e576fbb0
kiler398 <kiler398@gmail.com>
2013-04-17 03:07:24 +0000

    实现号码段Mongodb读取

---
+++ SNFrameworkCode/SNFramework/SNFramework/SPS.Bussiness/Cache/LinkIDNOSQLCache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SPS.Bussiness.Cache
+{
+    public class LinkIDNOSQLCache : ILinkIDCache
+    {
+        public void AddLinkIDs(string linkid, int channelid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckLinkIDIsExisted(string linkid, int channelid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
